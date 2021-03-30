adf0f98749bf066a73bed18d55c82f81372b89a8
Александр Пертенава <zwelenewskiy@yandex.ru>
2021-03-30 23:54:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DragonKingdom
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Прилижаемся к пещере...");
+                Console.ReadKey();
+
+                int chance = new Random().Next(1, 11);
+                if((chance >= 1) && (chance <= 8))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Вы попали в пещеру к дружелюбному дракону!");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Не повезло: Вы попали в пещеру со злым драконом дракону!");
+                    Console.ReadKey();
+
+                    Console.WriteLine("Игрок погиб! Конец игры...");
+                    Console.ReadKey();
+                    return;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
